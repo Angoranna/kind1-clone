2d1efecea91f9d142f8da9125bd6e476665dfdfc
Angoranna <annabelleangoran@gmail.com>
2017-07-06 20:09:34 +0000

    vrai version

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.bxslider1').bxSlider1({
+    slideWidth: 2500,
+    minSlides: 2,
+    maxSlides: 6,
+    auto:true
+    
+  });
+});
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    slideWidth: 110,
+    minSlides: 4,
+    maxSlides: 12,
+    auto:true
+    
+  });
+});
